77cf61f8fdc4c942895c29a26632b5dae4ac3d32
Thomas Rich <snowboarder2958@gmail.com>
2014-11-22 16:53:19 -0800

    first commit

---
+++ scraper.js
@@ -0,0 +1,17 @@
+var request = require('request');
+
+//get a single biobrick id either from the command line or another source
+//it will look like this: BBa_B0034
+
+
+
+
+//make an http request to http://parts.igem.org/cgi/xml/part.cgi?part=BBa_B0034
+request('http://parts.igem.org/cgi/xml/part.cgi?part=BBa_B0034', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log(body) // Print the google web page.
+  }
+})
+//get the response and parse it into a meaningful json object
+
+//
