d6257e1ec7235042d5db3504d3434f65694a3974
Thomas Rich <snowboarder2958@gmail.com>
2014-11-30 18:35:30 -0800

    adding example angular app

--- scraper.js
+++ scraper.js
@@ -9,7 +9,8 @@ var request = require('request');
 //make an http request to http://parts.igem.org/cgi/xml/part.cgi?part=BBa_B0034
 request('http://parts.igem.org/cgi/xml/part.cgi?part=BBa_B0034', function (error, response, body) {
   if (!error && response.statusCode == 200) {
-    console.log(body) // Print the google web page.
+    console.log(body) // Print the response body.
+    //parse the response to json
   }
 })
 //get the response and parse it into a meaningful json object
